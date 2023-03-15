59e0d83718f3c33fb6c27157cdb2691e019951e6
wadeking98 <wkingnumber2@gmail.com>
2023-03-15 15:14:54 -0700

    made snake prefer better connected tiles
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- src/search/graph.rs
+++ src/search/graph.rs
@@ -1,43 +1,27 @@
 use crate::{get_board_tile, logic, types};
 use std::collections::{HashMap, VecDeque};
 
-fn get_adj_tiles(
-    tile: &types::Coord,
-    board: &types::Board,
-    game_board: &HashMap<types::Coord, types::Flags>,
-    you: &types::Battlesnake,
-) -> Vec<types::Coord> {
-    let mut adj: Vec<types::Coord> = vec![];
-    for (.., dir) in types::DIRECTIONS.into_iter() {
-        let new_point = *dir + *tile;
-        if logic::can_move_board(&new_point, board, game_board, you, None) {
-            adj.push(new_point)
-        }
-    }
-    return adj;
-}
-
 pub fn bfs(
     board: &types::Board,
     game_board: &HashMap<types::Coord, types::Flags>,
     you: &types::Battlesnake,
-    food_connected_option: Option<u8>,
+    connection_threshold: f32,
 ) -> Vec<types::Coord> {
-    let food_connected = food_connected_option.unwrap_or(1);
     let mut frontier: VecDeque<types::Coord> = VecDeque::new();
     frontier.push_back(you.head);
     let mut visited: HashMap<types::Coord, types::Coord> = HashMap::new();
-    let mut current_path:Vec<types::Coord> = Vec::new();
-    let path_res = breadth_first_search_logic(
+    let goal_opt = breadth_first_search_logic(
         board,
         game_board,
         you,
-        &mut current_path,
         &mut frontier,
         &mut visited,
-        food_connected,
+        connection_threshold,
     );
-    return path_res;
+    return match goal_opt {
+      Some(goal) => backtrack(goal, &visited),
+      None => vec![]
+    };
 }
 
 fn backtrack(
@@ -74,36 +58,24 @@ fn breadth_first_search_logic(
     board: &types::Board,
     game_board: &HashMap<types::Coord, types::Flags>,
     you: &types::Battlesnake,
-    current_path: &mut Vec<types::Coord>,
     frontier: &mut VecDeque<types::Coord>,
     visited: &mut HashMap<types::Coord, types::Coord>,
-    mut food_connected: u8,
-) -> Vec<types::Coord> {
+    connection_threshold: f32
+) -> Option<types::Coord> {
     if frontier.len() <= 0 {
-        return vec![];
+        return None;
     }
 
     let current_tile = frontier.pop_front().unwrap();
 
-    if get_board_tile!(game_board, current_tile.x, current_tile.y) == types::Flags::FOOD && !current_path.contains(&current_tile) {
-        if food_connected <= 1 {
-            let mut best_path = backtrack(current_tile, &visited);
-            current_path.append(&mut best_path);
-            return current_path.clone();
-        } else {
-            let mut path = backtrack(current_tile, &visited);
-            current_path.append(&mut path);
-            visited.clear();
-            frontier.clear();
-            frontier.push_back(current_path[current_path.len()-1]);
-            food_connected -= 1;
-        }
+    if get_board_tile!(game_board, current_tile.x, current_tile.y) == types::Flags::FOOD {
+        return Some(current_tile);
     }
 
     // get adj tiles if they haven't been visited before and they're not in the current path
-    let adj_tiles: Vec<types::Coord> = get_adj_tiles(&current_tile, board, &game_board, you)
+    let adj_tiles: Vec<types::Coord> = logic::get_adj_tiles_connected(&current_tile, board, &game_board, you, connection_threshold, Some(true), None)
         .into_iter()
-        .filter(|tile| visited.get(tile).is_none() && !current_path.contains(tile))
+        .filter(|tile| visited.get(tile).is_none())
         .collect();
 
     // mark adj tiles as visited and link the parent node
@@ -116,15 +88,8 @@ fn breadth_first_search_logic(
     frontier.append(&mut adj_tiles_deque);
 
     // recursion step
-    let best_path =
-        breadth_first_search_logic(board, game_board, you, current_path, frontier, visited, food_connected);
+    return breadth_first_search_logic(board, game_board, you, frontier, visited, connection_threshold);
 
-    // if we can't find a food that is connected to n other foods, return a food that is connected to n-1 foods
-    if best_path.len() > 0 {
-        return best_path;
-    } else {
-        return current_path.clone();
-    }
 }
 
 #[cfg(test)]
@@ -176,7 +141,7 @@ mod test {
         let board: types::Board = serde_json::from_str(BOARD_DATA).unwrap();
         let you: types::Battlesnake = board.snakes[0].clone();
         let game_board = board.to_game_board();
-        let adj = get_adj_tiles(&you.head, &board, &game_board, &you);
+        let adj = logic::get_adj_tiles(&you.head, &board, &game_board, &you, None);
         assert!(
             adj.contains(&(you.head + types::DIRECTIONS["left"]))
                 && adj.contains(&(you.head + types::DIRECTIONS["right"]))
@@ -244,10 +209,8 @@ mod test {
         let you = board.snakes[0].clone();
         let mut game_board = board.to_game_board();
 
-        let path = bfs(&board, &mut game_board, &you, None);
+        let path = bfs(&board, &mut game_board, &you, 0.5);
         assert!(path.len() > 0 && path[path.len() - 1] == types::Coord { x: 8, y: 4 });
 
-        let path_connected = bfs(&board, &game_board, &you, Some(2));
-        assert!(path_connected.len() > 0 && path_connected[path_connected.len() - 1] == types::Coord { x: 4, y: 10 });
     }
 }
